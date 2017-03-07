124d245db7bd41e698616da047e40597fd82af7f
Hung Nguyen <nhoklangthang96@bitbucket.com>
2017-03-07 07:08:21 +0700

    commit 1

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNamelabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     
+    // 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
